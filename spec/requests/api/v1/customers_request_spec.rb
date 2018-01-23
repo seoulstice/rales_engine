a0c058adc82a9d102d19b41e4df378665fbe1e80
seoulstice <jungyoungjoon@gmail.com>
2018-01-23 13:45:02 -0700

    Merchant single/multi finders spec passing

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -19,10 +19,10 @@ describe "Customers API" do
 
       get "/api/v1/customers/#{id}"
 
-      item = JSON.parse(response.body)
+      item = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
-      expect(item["id"]).to eq(id)
+      expect(item[:id]).to eq(id)
     end
   end
 end
---
+++ spec/requests/api/v1/merchants/search_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  context "HTTP GET with Query Parameters" do
+    it "returns single Merchant by name search" do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+
+      get "/api/v1/merchants/find?name=#{merchant1.name}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant[:id]).to eq(merchant1.id)
+    end
+
+    it "returns multiple Merchants by name search" do
+      merchant1, merchant2, merchant3, merchant4 = create_list(:merchant, 4)
+
+      get "/api/v1/merchants/find_all?name=#{merchant1.name}"
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body)
+
+      expect(merchants.count).to eq(1)
+      expect(merchants.class).to eq(Array)
+    end
+  end
+end

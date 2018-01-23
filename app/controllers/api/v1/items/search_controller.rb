2bad01e80a1fa65996ecb58d55874fb02438b4f8
TMad <madsen.tyler@gmail.com>
2018-01-23 14:22:01 -0700

    update controllers for additional attributes

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,32 +2,44 @@ class Api::V1::Items::SearchController < ApplicationController
 
   def index
     if params[:id]
-      render json: Item.find(params[:id]), status: 200
+      render json: Item.find(params[:id])
     elsif params[:name]
-      render json: Item.where(name: params[:name]), status: 200
+      render json: Item.where(name: params[:name])
+    elsif params[:description]
+      render json: Item.where(description: params[:description])
+    elsif params[:unit_price]
+      render json: Item.where(unit_price: params[:unit_price])
+    elsif params[:merchant_id]
+      render json: Item.where(merchant_id: params[:merchant_id])
     elsif params[:created_at]
-      render json: Item.where(created_at: params[:created_at]), status: 200
+      render json: Item.where(created_at: params[:created_at])
     elsif params[:update_at]
-      render json: Item.where(updated_at: params[:updated_at]), status: 200
+      render json: Item.where(updated_at: params[:updated_at])
     end
   end
 
   def show
     if params[:id]
-      render json: Item.find(params[:id]), status: 200
+      render json: Item.find(params[:id])
     elsif params[:name]
-      render json: Item.find_by(name: params[:name]), status: 200
+      render json: Item.find_by(name: params[:name])
+    elsif params[:description]
+      render json: Item.find_by(description: params[:description])
+    elsif params[:unit_price]
+      render json: Item.find_by(unit_price: params[:unit_price])
+    elsif params[:merchant_id]
+      render json: Item.find_by(merchant_id: params[:merchant_id])
     elsif params[:created_at]
-      render json: Item.find_by(created_at: params[:created_at]), status: 200
+      render json: Item.find_by(created_at: params[:created_at])
     elsif params[:update_at]
-      render json: Item.find_by(updated_at: params[:updated_at]), status: 200
+      render json: Item.find_by(updated_at: params[:updated_at])
     end
   end
 
   private
 
     def item_params
-      params.require(:item).permit(:id, :name, :created_at, :update_at)
+      params.require(:item).permit(:id, :name, :description, :created_at, :update_at)
     end
 
 end

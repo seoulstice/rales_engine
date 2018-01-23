0c3b87a9a22559b1bac585b5a7ca0fb66ab64ddf
TMad <madsen.tyler@gmail.com>
2018-01-23 13:28:48 -0700

    update datetime in factories

--- spec/factories/customer.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :customer do
-    first_name "Tyler"
-    last_name "Madsen"
-    created_at Date.today
-    updated_at Date.today
-  end
-end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     merchant
     description "There's always money in the banana stand!"
     unit_price 75107
-    created_at Date.today
-    updated_at Date.today
+    created_at "2012-03-27 14:53:59 UTC"
+    updated_at "2012-03-27 14:53:59 UTC"
   end
 end

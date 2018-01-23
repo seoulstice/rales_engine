f933d0c7197559da46fa9171a37602c1807bc22e
seoulstice <jungyoungjoon@gmail.com>
2018-01-23 13:45:42 -0700

    cloned spec harness

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        controller :search do
+          get '/find' => :show
+          get '/find_all' => :index
+        end
+      end
       resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
       resources :customers, only: [:index, :show]

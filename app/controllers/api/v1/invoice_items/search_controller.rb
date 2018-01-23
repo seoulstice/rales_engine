2fcd372dcb8104255194be4a50db84c22b7f9243
TMad <madsen.tyler@gmail.com>
2018-01-23 16:17:50 -0700

    add invoice items search controller

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::InvoiceItems::SearchController < ApplicationController
 
+  def index
+    render json: InvoiceItem.where(invoice_item_params)
+  end
+
+  def show
+    render json: InvoiceItem.find_by(invoice_item_params)
+  end
+
+  private
+
+    def invoice_item_params
+      params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :update_at)
+    end
 
 end

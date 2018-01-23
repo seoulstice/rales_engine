8a8f5c2f2955a8210361196bc4493b3035c22a2d
TMad <madsen.tyler@gmail.com>
2018-01-23 12:11:00 -0700

    add namespace routes for find and find all

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,28 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :items do
+        controller :search do
+          get '/find' => :show
+          get '/find_all' => :show
+        end
+      end
       resources :items, only: [:index, :show]
+
+      namespace :invoices do
+        controller :search do
+          get '/find' => :show
+          get '/find_all' => :show
+        end
+      end
       resources :invoices, only: [:index, :show]
+
+      namespace :invoice_items do
+        controller :search do
+          get '/find' => :show
+          get '/find_all' => :show
+        end
+      end
       resources :invoice_items, only: [:index, :show]
     end
   end

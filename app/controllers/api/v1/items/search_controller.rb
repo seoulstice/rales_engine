185effab69981451370c1592d348858724a550ee
TMad <madsen.tyler@gmail.com>
2018-01-23 12:34:08 -0700

    add show find action for item controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::Items::SearchController < ApplicationController
 
+  def show
+    if params[:id]
+    elsif params[:name]
+      Item.find_by(name: params[:name])
+    elsif params[:created_at]
+    elsif params[:update_at]
+    end
+  end
 
 end

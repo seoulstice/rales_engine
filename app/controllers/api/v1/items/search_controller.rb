d62d60ad21f7ab241d25b4a9e2f1af7d786889ad
TMad <madsen.tyler@gmail.com>
2018-01-23 13:32:29 -0700

    add 200 status to item search controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,26 +2,32 @@ class Api::V1::Items::SearchController < ApplicationController
 
   def index
     if params[:id]
-      Item.find(params[:id])
+      render json: Item.find(params[:id]), status: 200
     elsif params[:name]
-      Item.where(name: params[:name])
+      render json: Item.where(name: params[:name]), status: 200
     elsif params[:created_at]
-      Item.where(created_at: params[:created_at])
+      render json: Item.where(created_at: params[:created_at]), status: 200
     elsif params[:update_at]
-      Item.where(updated_at: params[:updated_at])
+      render json: Item.where(updated_at: params[:updated_at]), status: 200
     end
   end
 
   def show
     if params[:id]
-      Item.find(params[:id])
+      render json: Item.find(params[:id]), status: 200
     elsif params[:name]
-      Item.find_by(name: params[:name])
+      render json: Item.find_by(name: params[:name]), status: 200
     elsif params[:created_at]
-      Item.find_by(created_at: params[:created_at])
+      render json: Item.find_by(created_at: params[:created_at]), status: 200
     elsif params[:update_at]
-      Item.find_by(updated_at: params[:updated_at])
+      render json: Item.find_by(updated_at: params[:updated_at]), status: 200
     end
   end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:id, :name, :created_at, :update_at)
+    end
 
 end

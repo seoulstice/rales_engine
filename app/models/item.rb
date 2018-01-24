7f9895ff2f7652c47c5113484e09372229674ea7
TMad <madsen.tyler@gmail.com>
2018-01-23 17:47:34 -0700

    add item most revenue active record method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_revenue(quantity)
+    select("items.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue")
+    .joins(:invoice_items)
+    .group(:id)
+    .order("revenue DESC")
+    .limit(quantity.to_i)
+  end
 end
+
+Item.select("items.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue").joins(:invoice_items).group(:id).order("revenue DESC").limit(2)

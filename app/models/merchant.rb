9512b13af315066ef6909975c7dbe3b200d7fb05
seoulstice <jungyoungjoon@gmail.com>
2018-01-24 15:09:20 -0700

    Create self.revenue for Merchant revenue from successful transactions

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,17 +1,23 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
   has_many :items
 
   def self.most_revenue(quantity)
     select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
-    .joins(:invoice_items)
-    .group(:id)
-    .order("revenue DESC")
-    .limit(quantity)
+      .joins(:invoice_items)
+      .group(:id)
+      .order("revenue DESC")
+      .limit(quantity)
   end
 
-  def revenue
-    .joins
+  def self.revenue(merchant_id)
+    select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+      .joins(:invoice_items, :invoices, :transactions)
+      .merge(Transaction.successful)
+      .group(:id)
+      .find(merchant_id)
   end
+
 end

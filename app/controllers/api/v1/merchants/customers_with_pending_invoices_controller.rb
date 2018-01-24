f9be1ded012c5ad252686feb37609b4f60fad558
TMad <madsen.tyler@gmail.com>
2018-01-24 15:34:28 -0700

    add customers ith pending invoices controller

--- app/controllers/api/v1/merchants/customers_with_pending_invoices_controller.rb
+++ app/controllers/api/v1/merchants/customers_with_pending_invoices_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Merchants::CustomersWithPendingInvoicesController < ApplicationController
 
   def show
-    require "pry"; binding.pry
     render json: Customer.customers_with_pending_invoices(params[:id])
   end
 

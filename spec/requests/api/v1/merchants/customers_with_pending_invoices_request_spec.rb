b59c5c1d3d81d4367cdd7af91283e5059dff36ba
TMad <madsen.tyler@gmail.com>
2018-01-24 15:34:38 -0700

    add customers with pending invoices request test

--- spec/requests/api/v1/merchants/customers_with_pending_invoices_request_spec.rb
+++ spec/requests/api/v1/merchants/customers_with_pending_invoices_request_spec.rb
@@ -4,12 +4,13 @@ describe "Customers with Pending Invoices API" do
   context "HTTP GET" do
     it "returns customers_with_pending_invoices for a single merchant" do
       merchant1 = create(:merchant)
-      customer1 = create(:customer)
-      customer2 = create(:customer)
-      customer3 = create(:customer)
-      create(:invoice, merchant: merchant1, customer: customer1, status: "pending")
-      create(:invoice, merchant: merchant1, customer: customer2)
-      create(:invoice, merchant: merchant1, customer: customer3, status: "pending")
+      customer1, customer2, customer3 = create_list(:customer, 3)
+      invoice1 = create(:invoice, merchant: merchant1, customer: customer1)
+      invoice2 = create(:invoice, merchant: merchant1, customer: customer2)
+      invoice3 = create(:invoice, merchant: merchant1, customer: customer3)
+      create(:transaction, invoice: invoice1, result: 'failed')
+      create(:transaction, invoice: invoice2, result: 'success')
+      create(:transaction, invoice: invoice3, result: 'failed')
 
       get "/api/v1/merchants/#{merchant1.id}/customers_with_pending_invoices"
 

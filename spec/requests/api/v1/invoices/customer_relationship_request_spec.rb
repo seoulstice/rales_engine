19b12a802a3b0caf051de65aa50e811c1ddeb961
seoulstice <jungyoungjoon@gmail.com>
2018-01-25 16:41:24 -0700

    Create invoice-customer relationship spec

---
+++ spec/requests/api/v1/invoices/customer_relationship_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+require 'rails_helper'
+
+describe "Invoice-Customer Relationship API" do
+  context "HTTP GET" do
+    it "returns all customer associated to single Invoice" do
+      customer = create(:customer, first_name: "John", last_name: "Doe")
+      invoice = create(:invoice, customer: customer)
+
+      get "/api/v1/invoices/#{invoice.id}/customer"
+
+      expect(response).to be_success
+
+      customer1 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(customer1[:id]).to eq(1)
+      expect(customer1[:first_name]).to eq("John")
+      expect(customer1[:last_name]).to eq("Doe")
+    end
+  end
+end

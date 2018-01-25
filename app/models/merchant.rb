56f9fbb85431458ef95ba35407418457d7c1056a
TMad <madsen.tyler@gmail.com>
2018-01-24 20:00:07 -0700

    remove transaction dependency from all revenue by date method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,7 +4,7 @@ class Merchant < ApplicationRecord
 
   def self.most_revenue(quantity)
     select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
-      .joins(:invoice_items)
+      .joins(invoices: [:invoice_items])
       .group(:id)
       .order("revenue DESC")
       .limit(quantity)
@@ -16,6 +16,7 @@ class Merchant < ApplicationRecord
       .merge(Transaction.successful)
       .group(:id)
       .find(merchant_id)
+      .revenue
   end
 
   def self.revenue_on_date(merchant_id, date)
@@ -25,6 +26,15 @@ class Merchant < ApplicationRecord
       .where(invoices: {created_at: date})
       .group(:id)
       .find(merchant_id)
+      .revenue
+  end
+
+  def self.all_revenue_by_date(date)
+      joins(invoices: [:invoice_items])
+      .where(invoices: {created_at: date})
+      .sum("invoice_items.quantity * invoice_items.unit_price")
   end
 
 end
+
+# Merchant.joins(invoices: [:invoice_items, :transactions]).merge(Transaction.successful).where(invoices: {created_at: date}).sum("invoice_items.quantity * invoice_items.unit_price")

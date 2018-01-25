4b9d2c76bfcac91d86bc19f5f5b1b2ef22aca655
TMad <madsen.tyler@gmail.com>
2018-01-24 19:59:46 -0700

    modify index view strong params

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def index
-    render json: Merchant.all_revenue_by_date(params[:date])
+    render json: Merchant.all_revenue_by_date(revenue_params[:date]), serializer: RevenueSerializer
   end
 
   def show
     if params[:date]
-      render json: Merchant.revenue_on_date(params[:id], params[:date]), serializer: RevenueSerializer
+      render json: Merchant.revenue_on_date(params[:id], revenue_params[:date]), serializer: RevenueSerializer
     else
       render json: Merchant.revenue(params[:id]), serializer: RevenueSerializer
     end

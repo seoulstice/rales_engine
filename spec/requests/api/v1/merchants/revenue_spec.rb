5049bfb50c7c4a05fb67a57c0a945fea3bd72267
TMad <madsen.tyler@gmail.com>
2018-01-24 17:54:02 -0700

    add all revenue by date request test

--- spec/requests/api/v1/merchants/revenue_spec.rb
+++ spec/requests/api/v1/merchants/revenue_spec.rb
@@ -33,5 +33,26 @@ describe "Merchants API" do
 
       expect(merchant[:revenue]).to eq("1.00")
     end
+    it "returns revenue for all merchants on a single date" do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+      invoice1 = create(:invoice, merchant: merchant1, created_at: "2012-03-16")
+      invoice2 = create(:invoice, merchant: merchant2, created_at: "2012-03-16")
+      invoice3 = create(:invoice, merchant: merchant3, created_at: "2012-01-02")
+      create(:invoice_item, invoice: invoice1, quantity: 2, unit_price: 100)
+      create(:invoice_item, invoice: invoice2, quantity: 1, unit_price: 200)
+      create(:invoice_item, invoice: invoice2, quantity: 1, unit_price: 300)
+      create(:invoice_item, invoice: invoice3, quantity: 1, unit_price: 400)
+
+
+      get "/api/v1/merchants/revenue?date=#{invoice1.created_at}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant[:revenue]).to eq("7.00")
+    end
   end
 end

97c24545959ecc25f53b259d0ecb841f8013c4a1
TMad <madsen.tyler@gmail.com>
2018-01-24 17:54:13 -0700

    add all revenue by date controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
+  def index
+    render json: Merchant.all_revenue_by_date(params[:date])
+  end
+
   def show
     if params[:date]
       render json: Merchant.revenue_on_date(params[:id], params[:date]), serializer: RevenueSerializer
@@ -14,6 +18,10 @@ class Api::V1::Merchants::RevenueController < ApplicationController
     # if params[:date]
     #   params[:date]
     # end
+    params.permit(:id, :date)
+  end
+
+  def revenue_params
     params.permit(:id, :date)
   end
 

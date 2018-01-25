64bd4acfb3adf59d7dcd4659612fe4dc6d6e40b9
TMad <madsen.tyler@gmail.com>
2018-01-25 09:28:07 -0700

    update actions to sort for spec harness

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,19 +1,17 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def index
-    render json: Item.where(item_params)
+    render json: Item.where(item_params).order(:id)
   end
 
   def show
-    render json: Item.find_by(item_params)
+    render json: Item.where(item_params).order(:id).limit(1).first
   end
 
   private
 
     def item_params
-      if params[:unit_price]
-        params[:unit_price] = params[:unit_price].to_f*100
-      end
+      params[:unit_price] = (params[:unit_price].to_f*100).round(0) if params[:unit_price]
       params.permit(:id, :name, :description, :merchant_id, :unit_price, :created_at, :update_at)
     end
 

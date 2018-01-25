88d91b12ef221b5dd6ea922d0f70b8b4c2ab6faf
seoulstice <jungyoungjoon@gmail.com>
2018-01-25 12:31:41 -0700

    Passing spec for item best_day

---
+++ spec/requests/api/v1/items/best_day_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "Item Best Day API" do
+  context "HTTP GET" do
+    it "returns the best sale date for an Item" do
+      item = create(:item)
+
+      invoice1 = create(:invoice, created_at: "2012-03-22 08:54:10 UTC")
+      invoice2 = create(:invoice, created_at: "2012-03-22 08:54:10 UTC")
+      invoice3 = create(:invoice, created_at: "2012-03-19 13:54:10 UTC")
+      invoice4 = create(:invoice, created_at: "2012-03-19 13:54:10 UTC")
+
+      invoice_item1 = create(:invoice_item, item: item, invoice: invoice1)
+      invoice_item2 = create(:invoice_item, item: item, invoice: invoice2)
+      invoice_item4 = create(:invoice_item, item: item, invoice: invoice3)
+      invoice_item5 = create(:invoice_item, item: item, invoice: invoice4)
+
+      create(:transaction, invoice: invoice1, result: "success")
+      create(:transaction, invoice: invoice1, result: "failed")
+      create(:transaction, invoice: invoice1, result: "failed")
+      create(:transaction, invoice: invoice2, result: "success")
+      create(:transaction, invoice: invoice2, result: "failed")
+      create(:transaction, invoice: invoice3, result: "success")
+      create(:transaction, invoice: invoice3, result: "failed")
+      create(:transaction, invoice: invoice3, result: "failed")
+      create(:transaction, invoice: invoice4, result: "success")
+      create(:transaction, invoice: invoice4, result: "failed")
+
+      get "/api/v1/items/#{item.id}/best_day"
+
+      expect(response).to be_success
+# binding.pry
+      days = JSON.parse(response.body, symbolize_names: true)
+
+      expect(days[:best_day]).to eq("2012-03-22T08:54:10.000Z")
+    end
+  end
+end
